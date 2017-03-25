e8604e4839685c0f43d800f645cb373239d68b48
Michal Adamiec <madamiec@mion.elka.pw.edu.pl>
2017-03-26 00:42:29 +0100

    test1

---
+++ Application.cpp
@@ -0,0 +1,8 @@
+#include "Application.hpp"
+#include <iostream>
+
+using namespace std;
+void Application::Run()
+{
+    cout<<"no elo";
+}
---
+++ Application.hpp
@@ -0,0 +1,10 @@
+#ifndef Application_hpp
+#define Application_hpp
+
+
+class Application
+{
+    public:
+    void Run();
+};
+#endif
