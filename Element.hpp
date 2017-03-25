e1491a6009bf8386b2a2dd02ea477bf1b3d59499
Michal Adamiec <madamiec@mion.elka.pw.edu.pl>
2017-03-26 00:57:07 +0100

    test4

--- Element.hpp
+++ Element.hpp
@@ -1,4 +1,11 @@
+
+
 class Element
 {
+    private:
+    char* type; // LED, compact, traditional, halogen
+    double lumens; // in lumens (lm)
+    double watts; 
+    int colour; // in kelvins (K)
     
-}
+};
